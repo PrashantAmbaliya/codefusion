0783c9b654066d873d4a1ea72c04fb84b1e2eddd
JustPrashh <prashantambaliya226@gmail.com>
2023-12-02 23:45:35 +0530

    First Commit

---
+++ src/pages/editorPage.jsx
@@ -0,0 +1,12 @@
+
+const EditorPage = () => {
+    return (
+        <>
+
+            <h1>Hello From Editor</h1>
+
+        </>
+    )
+}
+
+export default EditorPage;
---
+++ src/pages/home.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { v4 as uuidV4 } from "uuid";
+
+const Home = () => {
+  const [roomID, setRoomID] = useState("");
+  const [username, setUsername] = useState("");
+
+  const createRoom = (e) => {
+    e.preventDefault();
+    setRoomID(uuidV4());
+  };
+
+  return (
+    <>
+      <div className="homePageWrapper">
+        
+        <div className="formWrapper">
+          <img
+            className="HomePageLogo"
+            src="logo192.png"
+            alt="CodeFusion Logo"
+          />
+          <h4 className="mainLable">Enter Room ID</h4>
+          <div className="inputGroup">
+            <input
+              onChange={(e) => {
+                setRoomID(e.target.value);
+              }}
+              value={roomID}
+              type="text"
+            />
+            <input
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              value={username}
+              type="text"
+            />
+            <button></button>
+            <span className="createinfo">
+              <a href="" className="createRoom" onClick={createRoom}>Create a new Room</a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
